c6626322b68af7a934b043fc8549e266acd4a90d
SiKun <psikun@foxmail.com>
2022-08-31 10:34:31 +0800

    feat: 搭建项目基本框架

---
+++ src/main/java/com/emall/EmallApplication.java
@@ -0,0 +1,16 @@
+package com.emall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SiKun
+ */
+@SpringBootApplication
+public class EmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emall/EmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
