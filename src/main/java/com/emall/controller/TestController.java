b6ac08a9d168fa1977a82376374d597629c0f385
SiKun <psikun@foxmail.com>
2022-08-31 11:15:19 +0800

    feat: 添加测试Controller

---
+++ src/main/java/com/emall/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.emall.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author SiKun
+ * @Description TestController
+ * @date 2022/08/31/ 11:13
+ */
+
+@Slf4j
+@Controller
+public class TestController {
+}
