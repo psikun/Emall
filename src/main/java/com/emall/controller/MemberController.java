74f2ce7e5ee1c3a05d0c51683930702bb6975639
SiKun <psikun@foxmail.com>
2022-09-17 20:23:56 +0800

    feat: 会员管理相关方法

--- src/main/java/com/emall/controller/MemberController.java
+++ src/main/java/com/emall/controller/MemberController.java
@@ -12,14 +12,14 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/Member")
+@RequestMapping("/member")
 public class MemberController {
     @Autowired
     MemberService memberService;
 
     @ApiOperation("通过会员id获取会员信息")
     @GetMapping("/{memberId}")
-    public Result<Member> getMemberById(int memberId) {
+    public Result<Member> getMemberById(@PathVariable("memberId") int memberId) {
         Member member = memberService.getMemberById(memberId);
         if (member == null) {
             return Result.failed();
@@ -32,10 +32,11 @@ public class MemberController {
     @GetMapping("/list")
     public Result<List<Member>> list() {
         List<Member> list = memberService.list();
-        if (list == null)
+        if (list == null) {
             return Result.failed();
-        else
+        } else {
             return Result.success(list, "成功了");
+        }
     }
 
     @ApiOperation("增加会员信息")
@@ -56,12 +57,13 @@ public class MemberController {
         }
         return Result.failed("更新失败");
     }
+
     @ApiOperation("根据会员id删除会员")
     @DeleteMapping("/delete")
-    public Result<String> deleteById(int id){
+    public Result<String> deleteById(int id) {
         int delete = memberService.delete(id);
-        if(delete == 1){
-            return Result.success(null,"删除成功");
+        if (delete == 1) {
+            return Result.success(null, "删除成功");
         }
         return Result.failed("删除失败");
     }
